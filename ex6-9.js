2c05c5f32ff1c1747c3fdae470feefd1fdf124b8
kyosukeota <zm18242@josai.ac.jp>
2021-01-19 03:09:59 +0000

    1/19

--- ex6-9.js
+++ ex6-9.js
@@ -1,1 +1,3 @@
 function txtchange1(){document.getElementById('txt1').innerHTML='ナンバーワン!';}
+function imgchange1(){document.getElementById('logo').src='./image/bg_ocean_suiheisen_red.gif';}
+function imgchange2(){document.getElementById('logo').src='./image/josai.png';}
